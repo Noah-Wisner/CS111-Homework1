fb1c6ec4330c8fa12a1edc5a11d7357836c6494b
Noah-Wisner <wisnerboy1@gmail.com>
2024-10-25 16:06:20 -0700

    Now decrypts to int - Added printMessage (will itterate through a int vec - Added decryptToInt (iterates through a vector, applying largePowMod to   all slots in vector) - Added largePowMod (my greatest creation)

--- main.cpp
+++ main.cpp
@@ -12,11 +12,14 @@ void findPrimeFactors(int, int&, int&);
 int findTotient(int,int);
 int findModInverse(int, int);
 void EuclidianExtended(int, int, int&, int&);
+void decryptToInt(vector<int>&, int, int,int);
+int largePowMod(int base, int exp, int mod);
+void printMessage(vector<int>&,int);
 
 int main()
 {
  //input variables
- int m;
+ int m; //message length
  int input;
  vector<int> message;
 
@@ -49,8 +52,8 @@ t = findTotient(p,q);
 //step 3. find decryption key, d = inverse e mod t
 d = findModInverse(e,t);
 
-//step 4. decrypt code into integers
-
+//step 4. translate encryption -> decrypted integers
+decryptToInt(message, d,n,m);
 //step 5. translate integers -> text
 
 //DEBUGGING!!!
@@ -58,6 +61,8 @@ cout << "p = " << p << endl;
 cout << "q = " << q << endl;
 cout << "t = " << t << endl;
 cout << "d = " << d << endl;
+cout << "decrypted integer message : "; printMessage(message, m);
+
 
 }
 
@@ -120,7 +125,7 @@ void EuclidianExtended(int a, int b, int& x1, int& y1)
         return;
     }
 
-    //recurssive case
+    //recursive case
     int x2 = 0;
     int y2 = 0;
     EuclidianExtended(b % a, a, x2, y2);
@@ -129,4 +134,52 @@ void EuclidianExtended(int a, int b, int& x1, int& y1)
     y1 = x2;
 
     return;
+}
+
+void decryptToInt(vector<int>& message, int d, int n, int m)
+{
+    //D(C) = C^D mod n
+    for (int i=0; i < m ;i++)
+    {
+        message[i] = largePowMod(message[i],d,n);
+    }
+}
+
+void printMessage(vector<int>& message, int m)
+{
+    for (int i=0; i < m ;i++)
+    {
+        cout << message[i];
+    }
+    cout << endl;
+}
+
+int largePowMod(int base, int exp, int mod)
+{
+    /*
+    FORMAT (base)^exp % mod
+    1. if exponenet is odd
+        - result acts as a way to "pull out" an exponent!
+        - to keep this num low % mod!
+    2. if exponent is even
+        - reduce exponent 1/2
+        - increase base by 2
+    */
+    int result = 1;
+
+    //just in case base > mod! helps keep it withing int probably not needed
+    base = base % mod;
+
+    while (exp > 0) {
+        if (exp % 2 == 1) 
+        {
+            result = (result * base) % mod;
+            exp -= 1;
+            continue;
+        }
+        
+        exp = exp / 2;
+        base = (base * base) % mod;
+    }
+    return result;
 }
